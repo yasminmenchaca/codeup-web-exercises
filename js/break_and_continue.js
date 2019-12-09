e3094c644d74411916a83f61bb06124a1bc83824
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2019-12-09 10:32:01 -0600

    prior to walk through

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,18 +1,18 @@
 "use strict";
-console.log("hello from break_and_continue.js");
+// console.log("hello from break_and_continue.js");
 
 //TODO: 1. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input. 2. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
-var random = Math.floor((Math.random() * 50) + 1);
+var userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
 
-console.log("Random odd number to skip is: " + random);
+console.log("Your number is: " + userNumber);
 
 for (var i = 1; i < 100; i++) {
     if (i % 2 === 0) {
         continue;
     }
 
-    if (random === i) {
+    if (userNumber === i) {
         console.log("Yikes! Skipping number: " + i);
     } else {
         console.log("Here is an odd number " + i);
@@ -23,4 +23,3 @@ for (var i = 1; i < 100; i++) {
     }
 
 }
-
--- js/while.js
+++ js/while.js
@@ -30,25 +30,4 @@ console.log("hello from while.js");
 
 //TODO: An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.
 
-// This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
 
-// This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
-
-
-// var allCones = Math.floor(Math.random() * 50) + 50;
-//
-// console.log("We have a total of " + allCones + " cones");
-//
-// do {
-//     var purchasedCones = Math.floor(Math.random() * 5) + 1;
-//     if (allCones < purchasedCones) {
-//         continue;
-//     }
-//     allCones -= purchasedCones;
-//     console.log("We have sold " + purchasedCones + " cones. We have " + allCones + " left!");
-//     if (allCones === 0) {
-//         console.log("Yay! We sold them all!");
-//     }
-// } while (allCones !== 0);
