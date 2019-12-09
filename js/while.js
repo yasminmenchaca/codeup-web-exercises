ba999bde4172ff1e44253798068964dbedbd398f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2019-12-09 10:37:14 -0600

    -

--- js/while.js
+++ js/while.js
@@ -1,9 +1,7 @@
 "use strict";
-<<<<<<< HEAD
-console.log("hello from while.js");
-=======
 
 console.log("hello from while.js");
+
 
 //TODO:Create a while loop that uses console.log() to create the output shown below:
 // 2
@@ -34,26 +32,7 @@ console.log("hello from while.js");
 //TODO: An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.
 
 
-<<<<<<< HEAD
 // This expression will generate a random number between 1 and 5
 // Math.floor(Math.random() * 5) + 1;
 
 
-// var allCones = Math.floor(Math.random() * 50) + 50;
-//
-// console.log("We have a total of " + allCones + " cones");
-//
-// do {
-//     var purchasedCones = Math.floor(Math.random() * 5) + 1;
-//     if (allCones < purchasedCones) {
-//         continue;
-//     }
-//     allCones -= purchasedCones;
-//     console.log("We have sold " + purchasedCones + " cones. We have " + allCones + " left!");
-//     if (allCones === 0) {
-//         console.log("Yay! We sold them all!");
-//     }
-// } while (allCones !== 0);
->>>>>>> 9da25d0aec4d549919acc12975c583a1d5b3db21
-=======
->>>>>>> e3094c644d74411916a83f61bb06124a1bc83824
