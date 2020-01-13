9102c0d9d7485027146ac5b849364dfe38cae0f5
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-13 16:01:09 -0600

    in progress

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -39,6 +39,7 @@ function geocode(search, token) {
  *  })
  *
  */
+
 function reverseGeocode(coordinates, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
