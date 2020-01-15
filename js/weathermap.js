e1876d6d50bfa8fc19b479966865f3223c53bf74
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-15 08:36:16 -0600

    in progress

--- js/weathermap.js
+++ js/weathermap.js
@@ -505,7 +505,7 @@ $(document).ready(function () {
         var mapCity = reverseGeocodeMarker(lngLat, mapboxToken);
         mapCity.then(function (result) {
             $('.span-location').html(result)
-            console.log(result)
+            // console.log(result)
         });
     }
     marker.on('dragend', onDragEnd);
