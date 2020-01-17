5210d9bb7944833c93f935e7fabdbf862b718ea4
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-17 07:42:57 -0600

    completed

--- js/es6.js
+++ js/es6.js
@@ -33,11 +33,14 @@ const users = [
 
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable declared as `const`
 
+// my answer
 const name = 'yasmin';
 const email = 'yasmin@email.com';
 const languages = ['html', 'css', 'javascript'];
 
 // TODO: rewrite the object literal using object property shorthand
+
+//object property shorthand & my answer
 const user = {
     name,
     email,
@@ -49,18 +52,29 @@ users.push({
     email: email,
     languages: languages
 });
-// console.log(users);
 
 // TODO: replace `var` with `let` in the following variable declarations
+// my answer
 let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
+// users.forEach(function(user) {
+//     return emails.push(user.email);
+// });
 
+// my answer
 users.forEach(user => {
     emails.push(user.email);
 });
 
+
+
+// users.forEach(function(user) {
+//     return names.push(user.name);
+// });
+
+// my answer
 users.forEach(user => {
     names.push(user.name);
 });
@@ -72,24 +86,35 @@ users.forEach(function (user) {
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
 
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
 
-    const {name, email, languages} = user;
+    const {name, email, languages} = user; // my answer
 
     // TODO: rewrite the assignment below to use template strings
 
+    // my answer
     developers.push(`${name}'s email is ${email}. ${name} knows ${languages}`);
-
 });
+
+//     developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+// });
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
+// developers.forEach(function(developer) {
 
+// my answer
 for (let developer of developers) {
 
     // TODO: rewrite the assignment below to use template strings
 
+    //list += '<li>' + developer + '</li>';
+
+    // my answer
     list += `<li>${developer}</li>`;
 
 }
