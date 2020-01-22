31ccee0559e27b39b3beee2ca6c807ef8a5fd636
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-22 12:28:01 -0600

    lecture

--- js/data.js
+++ js/data.js
@@ -1,19 +1,4 @@
-const puppy = "Banjo";
+const multiply = (x, y) => x * y;
+let banana = "banana";
 
-const obj = {
-    someNumber: 42,
-    whichSideOfTheForce: 'The Dark Side',
-    arnie: 'Get to da choppa!',
-};
-
-const multiply = (num1, num2) => {
-    let sum = 0;
-    for (let i = 0; i < num2; i++) {
-        sum += num1;
-    }
-    return sum;
-};
-
-const add = (x, y) => x + y;
-
-module.exports = {obj, multiply, puppy, add};
+export default {multiply, banana};
