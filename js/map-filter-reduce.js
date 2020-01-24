ad9f928951166ac9a2f396ba54fba1c6f28b939b
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-24 09:26:48 -0600

    edited

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -94,15 +94,15 @@ let users = [
 
 //TODO BONUS: Use .reduce to get the unique list of languages from the list of users.
 
-let uniqueLang = users
-    .reduce((languages, usersLang) => {
-        usersLang.languages.forEach(function (language) {
-            if (languages.includes(language)) {
-            } else {
-                languages.push(language);
-            }
-        });
-        return languages
-    }, []);
+// let uniqueLang = users
+//     .reduce((languages, usersLang) => {
+//         usersLang.languages.forEach(function (language) {
+//             if (languages.includes(language)) {
+//             } else {
+//                 languages.push(language);
+//             }
+//         });
+//         return languages
+//     }, []);
 
-console.log(uniqueLang);
+// console.log(uniqueLang);
